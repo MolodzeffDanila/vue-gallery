647e2e6f2f69851a772ed04995ff257c6af3b80d
MolodzeffDanila <russgun1@mail.ru>
2025-09-17 00:23:31 +0300

    feat: added header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <Header msg="Welcome to the Vue.js App!" />
+  </div>
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  
 }
 
-nav {
-  padding: 30px;
+:root {
+  --index: calc(1vw + 1vh)
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+    margin: 0;
+    background-color: #1f1f1f;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <img class="header" src="../assets/header.png"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+  .header {
+    width: calc(var(--index) * 20);
+    -webkit-mask-image: radial-gradient(rgba(31,31,31,1) calc(var(--index) * 4), rgba(31,31,31,0) calc(var(--index) * 12));
+    
+    mask-image: radial-gradient(rgba(31,31,31,1) calc(var(--index) * 4), rgba(31,31,31,0) calc(var(--index) * 12));
+  }
+</style>
